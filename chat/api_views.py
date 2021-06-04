688297b75bc4ef1270f92c94cfe17af373198789
t1m4 <731ruslan00@mail.ru>
2021-06-04 16:17:08 +0300

    Add api point for getting users

---
+++ chat/api_views.py
@@ -0,0 +1,29 @@
+from asgiref.sync import sync_to_async
+from django.contrib.auth.models import User
+from django.http import HttpResponse, JsonResponse
+from rest_framework.response import Response
+
+from chat.serializers import UserSerializer
+from login.auth_views import AsyncView, MyLoginRequiredMixin
+
+
+class UserView(MyLoginRequiredMixin):
+    result = {'status': 'ok', 'users': []}
+    async def get(self, request, *args, **kwargs):
+        try:
+            size = int(request.GET.get('size'))
+        except:
+            size = 10
+        size = 10 if size > 10 else size
+        users = await self.get_users(size)
+        serializer = UserSerializer(users, many=True)
+        data = await self.get_serializer_data(serializer)
+        self.result['users'] = data
+        return JsonResponse(self.result)
+
+    @sync_to_async
+    def get_users(self, size, *args, **kwargs):
+        return User.objects.filter(is_active=True).order_by('-id')[0:size]
+    @sync_to_async
+    def get_serializer_data(self, serializer, *args, **kwargs):
+        return serializer.data
